b6176a62b587df629b5e4ad975d19a29b85afc04
Eric Scuccimarra <skooch@gmail.com>
2017-01-23 16:23:24 +0000

    initial commit

---
+++ src/translateServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace escuccim\translate;
+
+use Illuminate\Support\ServiceProvider;
+
+class translateServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}

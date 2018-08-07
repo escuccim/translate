cba6c5bb08af256682d152d737e468e591ef16a7
Eric Antoine Scuccimarra <skooch@gmail.com>
2018-08-07 15:43:33 +0200

    Update translate.php

--- src/config/translate.php
+++ src/config/translate.php
@@ -5,15 +5,18 @@ return [
     'languages'     => [
         'en' => 'en',
         'fr' => 'fr',
+        'de' => 'de',
     ],
     'subdomains'    => [
         'fr'    => 'fr',
         'ch'    => 'fr',
+        'de'    => 'de',
         'www'   => 'en',
         'default'   => 'en',
     ],
     'date_formats'  => [
         'en'  => 'en_US_POSIX',
         'fr'  => 'fr_CH.UTF-8',
+        'de'  => 'fr_CH.UTF-8',
     ],
 ];
